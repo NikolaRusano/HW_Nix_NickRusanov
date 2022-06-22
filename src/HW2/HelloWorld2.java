f2a387b153c2eefec133462887696c18909fffdc
nick_note <nikolarusano92@gmail.com>
2022-06-22 22:49:53 +0300

    Secondcommit_Branch2_addedtext

--- src/HW2/HelloWorld2.java
+++ src/HW2/HelloWorld2.java
@@ -3,5 +3,6 @@ package HW2;
 public class HelloWorld2 {
     public static void main(String[] args) {
         System.out.println("HelloWorld2");
+        System.out.println("dsjfooenmkjkjvdojfd");
     }
 }
