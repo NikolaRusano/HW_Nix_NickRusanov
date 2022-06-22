f77846d66891f601e36d0446c8afa4bb332b8498
Nikolay Rusanov <nikolarusano92@gmail.com>
2022-06-22 23:50:30 +0300

    Branch2master_addedtext2

--- src/HW2/HelloWorld.java
+++ src/HW2/HelloWorld.java
@@ -3,5 +3,6 @@ package HW2;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Monkey");
+        System.out.printf("qwwweeeeeeeee");
     }
 }
